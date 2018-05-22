c8a838601de9e67aa5c2575fcb74573cea1e550a
bspotf <33741582+bspotf@users.noreply.github.com>
2018-05-22 12:47:47 +0700

    Update ClientApp.java

--- src/main/java/ru/client/ClientApp.java
+++ src/main/java/ru/client/ClientApp.java
@@ -1,8 +1,5 @@
 package ru.client;
 
-/**
- * Created by V on 01.03.2018.
- */
 public class ClientApp {
     public static void main(String[] args) throws InterruptedException {
         new Client().start(args);
